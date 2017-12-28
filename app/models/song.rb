e9ec47900693506dd3bfd3bc6e8aedc1c3ec0f72
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-28 21:20:15 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -22,7 +22,7 @@ class Song < ActiveRecord::Base
   def note_contents=(contents)
     contents.each do |content|
       note = Note.create(content: content)
-      self.notes << note
+      self.notes << note if !note.empty?
     end
   end
 
